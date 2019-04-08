88f0a69a848bf65f3e15c8bdfc914015d21064f5
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-04-08 17:46:57 +0600

    changes

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 const navLinks = document.querySelectorAll("nav a");
 const articles = document.querySelectorAll(".content");
-let drawer = document.querySelector(".drawer");
-let burger = document.querySelector(".burger");
 let activeLink = navLinks[0]; // Home
 
 navLinks.forEach(function(navLink) {
@@ -23,15 +21,6 @@ navLinks.forEach(function(navLink) {
 
     event.preventDefault();
   });
-});
-let boll = false;
-burger.addEventListener("click", function() {
-  boll = !boll;
-  if (boll === true) {
-    drawer.style.transform = "translateX(0)";
-  } else {
-    drawer.style.transform = "translateX(-100%)";
-  }
 });
 
 const input = document.querySelectorAll("form input");
@@ -85,31 +74,3 @@ linkMailto.addEventListener("click", function() {
   console.log(linkMailto.href);
 });
 console.log(linkMailto.href);
-
-var highlight = false;
-var cover = null;
-function backdrop(element) {
-  if (highlight == false) {
-    /*Create a transparent cover for the viewport*/
-    cover = document.createElement("div");
-    cover.style.height = "100%";
-    cover.style.width = "100%";
-    cover.style.backgroundColor = "black";
-    cover.style.opacity = "0.8";
-    cover.style.position = "fixed";
-    cover.style.top = "0px";
-    cover.style.left = "0px";
-    cover.style.zIndex = "1";
-
-    document.body.appendChild(cover);
-
-    /*Element has to be positioned so that we can apply z-index css property on it.*/
-    element.style.position = "relative";
-    element.style.zIndex = "2";
-    highlight = true;
-  } else {
-    document.body.removeChild(cover);
-    highlight = false;
-    element.style.zIndex = "0";
-  }
-}
