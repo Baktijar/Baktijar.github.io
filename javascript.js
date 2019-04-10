23ca056b01b22b93e2c69607a598182b5b26478d
Baktijar Anapijaev <Baktiar2794@gmail.com>
2019-04-10 16:08:17 +0600

    aksnaks

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const navLinks = document.querySelectorAll("nav a");
 const articles = document.querySelectorAll(".content");
+
 let activeLink = navLinks[0]; // Home
 
 navLinks.forEach(function(navLink) {
@@ -74,3 +75,36 @@ linkMailto.addEventListener("click", function() {
   console.log(linkMailto.href);
 });
 console.log(linkMailto.href);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
